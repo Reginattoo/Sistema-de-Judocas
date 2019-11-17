de5ac098da45176729a872c6608b5f6a46f3ee94
Reginattoo <55816455+Reginattoo@users.noreply.github.com>
2019-11-17 16:00:20 -0300

    testeCI

--- src/org/fpij/jitakyoei/view/gui/ProfessorCadastrarPanel.java
+++ src/org/fpij/jitakyoei/view/gui/ProfessorCadastrarPanel.java
@@ -15,7 +15,7 @@ import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
 /**
- * @author wansoul
+ * @author 31753477
  */
 public class ProfessorCadastrarPanel extends JPanel {
 	/**
@@ -100,4 +100,5 @@ public class ProfessorCadastrarPanel extends JPanel {
 	private JButton cadastrar;
 	private JButton cancelar;
 	// JFormDesigner - End of variables declaration  //GEN-END:variables
+        //Coment para meter o loko
 }
